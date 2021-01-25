e3af8842ec32d8fea6ffcc227ed6650748a1ab47
Emma Ruth Daly <60703785+EmmaD194@users.noreply.github.com>
2021-01-25 22:59:46 +0000

    added rich link preview and contact form

---
+++ contact-form-process.php
@@ -0,0 +1,23 @@
+<?php
+if (isset($_POST['email'])) {
+
+    $name = $_POST['name']; 
+    $email = $_POST['email']; 
+    $message = $_POST['message']; 
+
+    $email_to = "emma@gamesasgaeilge.com";
+    $headers = "From: ".$email;
+    $txt =  "You have a contact form submission from ".$name.".\n\n".$message;
+    $email_subject = "New contact form submission";
+
+ 
+    mail($email_to,$email_subject,$txt, $headers);
+    echo "Go raibh maith agat!" . " -" . "<a href='index.html' style='text-align:center;  text-decoration:none;color:#ff0099;'> Return to Games as Gaeilge!</a>";
+    
+
+}
+?>
+
+
+
+
