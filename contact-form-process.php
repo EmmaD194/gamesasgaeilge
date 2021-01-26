c64a9c0ebfd2ec90daa286c6e3830690052e08f9
Emma Ruth Daly <60703785+EmmaD194@users.noreply.github.com>
2021-01-26 13:29:17 +0000

    added privacy policy

--- contact-form-process.php
+++ contact-form-process.php
@@ -4,13 +4,14 @@ if (isset($_POST['email'])) {
     $name = $_POST['name']; 
     $email = $_POST['email']; 
     $message = $_POST['message']; 
+    $checkbox = $_POST['gdpr'];
 
     $email_to = "emma@gamesasgaeilge.com";
     $headers = "From: ".$email;
     $txt =  "You have a contact form submission from ".$name.".\n\n".$message;
     $email_subject = "New contact form submission";
 
- 
+   
     mail($email_to,$email_subject,$txt, $headers);
     echo "Go raibh maith agat!" . " -" . "<a href='index.html' style='text-align:center;  text-decoration:none;color:#ff0099;'> Return to Games as Gaeilge!</a>";
     
